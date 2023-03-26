c96c316d9ab3515370a0566cd1fd5919513f0383
Gianluca <gianluca.nulima@gmail.com>
2023-03-25 21:58:53 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,22 @@
+// função para exbir um alerta
+
+function ExibirAlerta() {
+    alert("ATENÇÃO! ESTE É UM ALERTA!");
+}
+
+function TrocarCorVerde() {
+    var elemento = document.getElementById("meuID");
+    elemento.style.backgroundColor = 'green';
+
+}
+
+function TrocarCorAmarelo() {
+    var elemento = document.getElementById("meuID");
+    elemento.style.backgroundColor = 'yellow';
+}
+
+function TrocarCorVermelho() {
+    var elemento = document.getElementById("meuID");
+    elemento.style.backgroundColor = 'red';
+}
+
